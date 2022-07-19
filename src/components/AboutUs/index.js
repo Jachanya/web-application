cc505cb719c87a04f58c609347dc36d91e76b1a5
jachan <johnachanya9@gmail.com>
2022-07-19 04:10:41 +0200

    changing other to order

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -16,7 +16,7 @@ const AboutUs = () => {
             high quality</span> service and <span style ={{color: "#C5B358"}}>
             smooth connection</span> between customers 
             and your business. We make it super easy for new customers to find your restaurant
-             and more easier for your current customers to other more.
+             and more easier for your current customers to order more.
             We provide No Stress or Struggles, Easy Life!
       </AboutUsPara>
     </AboutUsContainer>
