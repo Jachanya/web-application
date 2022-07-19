704dffb50d52ba502f4d2b3c6094caaaaa307763
jachan <johnachanya9@gmail.com>
2022-07-19 15:21:22 +0200

    added some stuff

--- src/components/AboutUs/AboutUsElements.js
+++ src/components/AboutUs/AboutUsElements.js
@@ -22,3 +22,54 @@ export const AboutUsPara = styled.p`
         margin: 20px;
     }
 `
+
+export const HowItWork = styled.div`
+    display: flex;
+    margin-top: 4rem;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    @media screen and ( max-width: 800px){
+        flex-direction: column;
+    }
+`
+
+export const HowItWorkH3 = styled.h2`
+    width: 100%;
+    font-size: 2rem;
+    margin: 1rem;
+    text-align: center;
+    font-family: 'Roboto', sans-serif;
+`
+
+export const BulletPointList = styled.div`
+    display: flex;
+    min-width: 800px;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 3rem;
+    @media screen and ( max-width: 800px){
+        flex-direction: column;
+        gap: 4rem;
+        padding-top: 4rem;
+    }
+`
+
+
+export const BulletPoint = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-family: 'Roboto', sans-serif;
+    align-items: center;
+    gap: 1rem;
+    min-width: 360px;
+`
+
+export const BulletPointH3 = styled.h3`
+    font-size: 1.4rem;
+`
+
+export const BulletPointP = styled.p`
+    max-width: 360px;
+    font-size: 1.3rem;
+`
--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {BiRestaurant} from 'react-icons/bi';
+import {AiOutlineAppstoreAdd} from 'react-icons/ai';
+import {CgListTree} from 'react-icons/cg';
+import {SiSimpleanalytics} from 'react-icons/si';
+
 import {
     AboutUsContainer,
     Header,
-    AboutUsPara
+    AboutUsPara,
+    HowItWork,
+    HowItWorkH3,
+    BulletPoint,
+    BulletPointList,
+    BulletPointP,
+    BulletPointH3
 } from './AboutUsElements';
 
 const AboutUs = () => {
@@ -10,17 +21,64 @@ const AboutUs = () => {
     <AboutUsContainer>
       <Header>
         <span style ={{color: "#9433FF"}}>
-            About</span> Us</Header>
+            About</span> us</Header>
       <AboutUsPara>
-        AFoodo provide <span style ={{color: "#C5B358"}}>
-            high quality</span> service and <span style ={{color: "#C5B358"}}>
+        AFoodo provide <span style ={{
+            color: "#C5B358"}}>
+            high quality service</span> and <span style ={{
+                color: "#C5B358"}}>
             smooth connection</span> between customers 
             and your business. We make it super easy for new customers to find your restaurant
              and more easier for your current customers to order more.
-            We provide No Stress or Struggles, Easy Life!
+            We provide no stress or struggles, we provide Easy Life!
       </AboutUsPara>
+      <HowItWork>
+        <HowItWorkH3>
+            How it work
+        </HowItWorkH3>
+        <BulletPointList>
+            
+            <BulletPoint>
+                <BiRestaurant
+                    size ={50}
+                    color= "#9433FF"
+                />
+                <BulletPointH3>Register your restaurant</BulletPointH3>
+                <BulletPointP>Once you have applied to be a business partner, 
+                    we would evaluate your submission and promptly respond.</BulletPointP>
+            </BulletPoint>
+            <BulletPoint>
+                <AiOutlineAppstoreAdd
+                    size ={50}
+                    color= "#9433FF"
+                />
+                <BulletPointH3>Add available food</BulletPointH3>
+                <BulletPointP>Once you have applied to be a business partner, 
+                    we would evaluate your submission and promptly respond.</BulletPointP>
+            </BulletPoint>
+            <BulletPoint>
+                <CgListTree
+                    size ={50}
+                    color= "#9433FF"
+                />
+                <BulletPointH3>List then for sale</BulletPointH3>
+                <BulletPointP>Once you have applied to be a business partner, 
+                    we would evaluate your submission and promptly respond.</BulletPointP>
+            </BulletPoint>
+            <BulletPoint>
+                <SiSimpleanalytics
+                    size ={50}
+                    color= "#9433FF"
+                />
+                <BulletPointH3>check out business analytics</BulletPointH3>
+                <BulletPointP>Once you have applied to be a business partner, 
+                    we would evaluate your submission and promptly respond.</BulletPointP>
+            </BulletPoint>
+        </BulletPointList>
+      </HowItWork>
     </AboutUsContainer>
   )
 }
 
 export default AboutUs
+
