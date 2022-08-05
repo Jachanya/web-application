390098a47f97de5704502f83e0f3729e93cb88a2
jachan <johnachanya9@gmail.com>
2022-08-05 14:01:28 +0200

    lets make some tiny changes

--- src/components/Forms/FormDeliver/FormElements.js
+++ src/components/Forms/FormDeliver/FormElements.js
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 export const FormContainer = styled.div`
+    padding: 5rem;
 `
 
 export const FormRest = styled.form`
     display: flex;
     flex-direction: column;
+    gap: 0.5rem;
+    justify-content: center;
 `
