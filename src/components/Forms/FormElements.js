f0ab17ac75a60f2812883d317c204d9e58df7202
jachan <johnachanya9@gmail.com>
2022-08-06 15:51:13 +0200

    just some sexy border radius

--- src/components/Forms/FormElements.js
+++ src/components/Forms/FormElements.js
@@ -29,6 +29,7 @@ export const Input = styled.input`
     outline: none;
     border: none;
     padding-left: 1rem;
+    border-radius: 5px;
     font-family: 'Roboto', sans-serif;
     font-size: 1rem;
     box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
